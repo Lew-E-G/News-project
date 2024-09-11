e2869749418cec421cf0e656996593a6f3373c89
Lewis <luigizzle@gmail.com>
2024-09-12 00:02:26 +0200

    Major changes: create additional category (theft) creation of new table and database layout with additional request to APIs

--- API_News.py
+++ API_News.py
@@ -12,7 +12,7 @@ url = 'https://newsapi.org/v2/everything'
 seven_days_ago_str = (datetime.now() - timedelta(days=30)).strftime('%Y-%m-%d')
 
 # Parameters for the API request
-params = {
+knife_params = {
     'q': '"knife" OR "blade" OR "stab" OR "stabbing" OR "stabbed"',
     'apiKey': api_key,
     'language': 'en',
@@ -21,9 +21,9 @@ params = {
     'pageSize': 100,  # Number of articles to fetch
     'sources': 'bbc-news'  # You can specify specific sources
 }
-def fetch_bbc():
+def fetch_knife_bbc():
     # Make the API request
-    response = requests.get(url, params=params)
+    response = requests.get(url, params=knife_params)
 
     # Shows remaining requests do this key
     # rate_limit_remaining = response.headers.get('X-Cache-Remaining')
@@ -59,5 +59,46 @@ def fetch_bbc():
     else:
         print(f"Failed to retrieve news. Status code: {response.status_code}")
 
+theft_params = {
+    'q': '"theft" OR "stole" OR "stolen" OR "robbery"',
+    'apiKey': api_key,
+    'language': 'en',
+    'dateStart': seven_days_ago_str,
+    'sortBy': 'relevancy',
+    'pageSize': 100,  # Number of articles to fetch
+    'sources': 'bbc-news'  # You can specify specific sources
+}
+
+def fetch_theft_bbc():
+    # Make the API request
+    response = requests.get(url, params=theft_params)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        results = response.json()['articles']
+        # Prints all info (pretty)
+        # print(json.dumps(articles, indent=4))
+        
+        # Create a list to store the fetched articles
+        articles = []
+
+        # Display the fetched news articles
+        for article in results:
+            article_data = {
+                'API':'BBC',
+                'published_at': article['publishedAt'],
+                'title': article['title'],
+                # 'body': article['fields'].get('body', 'No body content available'),
+                'url': article['url']
+            }
+            #for testing to see what article data was fetched
+            #print(article_data)
+            articles.append(article_data)
+        return articles
+    else:
+        print(f"Failed to retrieve news. Status code: {response.status_code}")
+
 if __name__ == '__main__':
-    fetch_bbc()
+    fetch_knife_bbc()
+    fetch_theft_bbc()
--- main.py
+++ main.py
@@ -1,45 +1,63 @@
-from API_Guardian import fetch_guardian
-from API_News import fetch_bbc
+from API_Guardian import fetch_knife_guardian, fetch_theft_guardian
+from API_News import fetch_knife_bbc, fetch_theft_bbc
 from cleanup import order_articles_csv, order_articles_db
 import pandas as pd
 import sqlite3
 import os
 
 #variables for the stored data
-bbc_data = fetch_bbc()
-guardian_data = fetch_guardian()
+bbc_knife_data = fetch_knife_bbc()
+guardian_knife_data = fetch_knife_guardian()
+bbc_theft_data = fetch_theft_bbc()
+guardian_theft_data = fetch_theft_guardian()
 
-#create DataFrames for the fetched data
-df_bbc = pd.DataFrame(bbc_data)
-df_guardian = pd.DataFrame(guardian_data)
+def merge_df(bbc, guardian):
+    #create DataFrames for the fetched data
+    df_bbc = pd.DataFrame(bbc)
+    df_guardian = pd.DataFrame(guardian)
 
-#combine the dataframes and drop duplicates
-df_allArticles = pd.concat([df_bbc,df_guardian], ignore_index=True).drop_duplicates(subset='url')
+    #combine the dataframes and drop duplicates
+    df_allArticles = pd.concat([df_bbc,df_guardian], ignore_index=True).drop_duplicates(subset='url')
+
+    #fills in missing data
+    df_allArticles['title'].fillna('No headline available', inplace=True)
+    df_allArticles['published_at'].fillna('No date available', inplace=True)
+    df_allArticles['url'].fillna('No URL available', inplace=True)
+    return df_allArticles
+
+df_allKnifeArticles = merge_df(bbc_knife_data, guardian_knife_data)
+df_allTheftArticles = merge_df(bbc_theft_data, guardian_theft_data)
 
 #DB file
 db_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'instance', 'cleaned_articles.db')
 conn = sqlite3.connect(db_file)
 cursor = conn.cursor()
 
-#combine the dataframes and drop duplicates
-df_allArticles = pd.concat([df_bbc,df_guardian], ignore_index=True).drop_duplicates(subset='url')
+# Common table structure
+table_structure = '''
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    api TEXT,
+    published_at NUMERIC,
+    title TEXT,
+    url TEXT
+'''
 
-#fills in missing data
-df_allArticles['title'].fillna('No headline available', inplace=True)
-df_allArticles['published_at'].fillna('No date available', inplace=True)
-df_allArticles['url'].fillna('No URL available', inplace=True)
-
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS articles_table (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        api TEXT,
-        published_at NUMERIC,
-        title TEXT,
-        url TEXT 
+# Knife crime table
+cursor.execute(f'''
+    CREATE TABLE IF NOT EXISTS knife_table (
+        {table_structure}
     )
 ''')
 
-df_allArticles.to_sql('articles_table', conn, if_exists='append', index=False)
+# Theft news table
+cursor.execute(f'''
+    CREATE TABLE IF NOT EXISTS theft_table (
+        {table_structure}
+    )
+''')
+#append data to relevant table 
+df_allKnifeArticles.to_sql('knife_table', conn, if_exists='append', index=False)
+df_allTheftArticles.to_sql('theft_table', conn, if_exists='append', index=False)
 
 conn.close()
 # Specify the CSV file name
