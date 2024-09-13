de46e5a7b0aa115976cea6b9c47e0bab901b742b
Lewis <luigizzle@gmail.com>
2024-09-13 16:59:04 +0200

    create function for db cleaning which main will call

--- cleanup.py
+++ cleanup.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import sqlite3
+import os
+
+db_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'instance', 'cleaned_articles.db')
+conn = sqlite3.connect(db_file)
+cursor = conn.cursor()
 
 def order_articles_csv(csv_file):
     # Read the CSV file into a DataFrame
@@ -21,21 +26,19 @@ def order_articles_csv(csv_file):
     # Save the modified DataFrame back to the CSV file
     df_sorted_unique.to_csv(csv_file, index=False)
 
-def order_articles_db(db_file):
-    # Connect to SQLite database
-    conn = sqlite3.connect(db_file)
+def order_articles_db(tables):
+    for table in tables:
+        # Step 1: Read the data from each table into a Pandas DataFrame
+        df = pd.read_sql(f'SELECT * FROM {table}', conn)
+        
+        # Step 2: Remove duplicates based on the 'url' column
+        df_cleaned = df.drop_duplicates(subset=['url'])
 
-    # Step 1: Read the existing data from the table into a Pandas DataFrame
-    df = pd.read_sql('SELECT * FROM articles_table ORDER BY published_at', conn)
-
-    # Step 2: Remove duplicates
-    df_cleaned = df.drop_duplicates(subset=['url'])
-
-    # Step 3: Write the cleaned data back to the SQL table, replacing the old data
-    df_cleaned.to_sql('articles_table', conn, if_exists='replace', index=False)
-
-    # Close the connection when done
-    conn.close()
+        # Order by date the article was published
+        df_ordered = df_cleaned.sort_values(by='published_at')
+        
+        # Step 3: Write the cleaned DataFrame back to the SQLite table (replace the old data)
+        df_ordered.to_sql(table, conn, if_exists='replace', index=False)
 
 if __name__ == '__main__':
     order_articles_db()
