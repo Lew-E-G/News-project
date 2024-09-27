0a42de63d0a0ae6c5b63496787237d2f4447903b
Lewis <luigizzle@gmail.com>
2024-09-27 22:06:46 +0100

    Major changes: JS fade in added Background image added Scroll snapping futher CSS updates

---
+++ static/JS/app.js
@@ -0,0 +1,29 @@
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         console.log(entry)
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('show');
+//         } else {
+//             entry.target.classList.remove('show');
+//         }
+//     });
+// });
+
+// const hiddenElements = document.querySelectorAll('.hidden')
+// hiddenElements.forEach((el) => observer.observe(el));
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.remove('show'); // Remove class to reset animation
+            void entry.target.offsetWidth; // Trigger reflow to ensure the animation resets
+            entry.target.classList.add('show'); // Add class to start the animation
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
