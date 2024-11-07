0af6ff9491b83d7ab60036eb4ffe80b9ba222662
Lewis <luigizzle@gmail.com>
2024-11-07 14:59:41 +0100

    adding the functions for the API calls of the drugs articles

--- API_Guardian.py
+++ API_Guardian.py
@@ -96,7 +96,48 @@ def fetch_theft_guardian():
         print(f"Failed to fetch data: {response.status_code}")
         return []  # Return an empty list in case of failure
     
+# Define the parameters for the request for news on drugs
+drug_params = {
+    'q': '"drug" OR "drugs" OR "narcotics"' ,  # Search for the keyword 'durg crime'
+    'section': 'uk-news',  # Specify the section for UK news
+    'from-date': seven_days_ago_str,  # Optional: Set the start date for the articles
+    'to-date': current_date_str,  # Optional: Set the end date for the articles
+    'api-key': api_key,  # Your API key
+    'page-size': 100,  # Optional: Number of results per page (can adjust as needed)
+    #'order-by': 'relevance',  # Optional: Order results by relevance
+    'show-fields': 'headline,webPublicationDate,shortUrl,body'  # Optional: Fields to include in the response
+}
+
+def fetch_drug_guardian():
+    response = requests.get(endpoint, params=drug_params)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        data = response.json()  # Parse the response JSON
+        results = data.get('response', {}).get('results', [])
+        
+        # Create a list to store the fetched articles
+        articles = []
+
+        # Append each article's details to the list
+        for article in results:
+            article_data = {
+                'API':'Guardian',
+                'published_at': article['webPublicationDate'],
+                'title': article['webTitle'],
+                # 'body': article['fields'].get('body', 'No body content available'),
+                'url': article['webUrl']
+            }
+            articles.append(article_data)
+        #print(articles)
+        return articles  # Return the list of articles
+
+    else:
+        print(f"Failed to fetch data: {response.status_code}")
+        return []  # Return an empty list in case of failure
+    
 
 if __name__ == '__main__':
     fetch_knife_guardian()
     fetch_theft_guardian()
+    fetch_drug_guardian()
--- API_News.py
+++ API_News.py
@@ -99,6 +99,47 @@ def fetch_theft_bbc():
     else:
         print(f"Failed to retrieve news. Status code: {response.status_code}")
 
+drug_params = {
+    'q': '"drug" OR "drugs" OR "narcotics"',
+    'apiKey': api_key,
+    'language': 'en',
+    'dateStart': seven_days_ago_str,
+    'sortBy': 'relevancy',
+    'pageSize': 100,  # Number of articles to fetch
+    'sources': 'bbc-news'  # You can specify specific sources
+}
+
+def fetch_drug_bbc():
+    # Make the API request
+    response = requests.get(url, params=drug_params)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        results = response.json()['articles']
+        # Prints all info (pretty)
+        # print(json.dumps(articles, indent=4))
+        
+        # Create a list to store the fetched articles
+        articles = []
+
+        # Display the fetched news articles
+        for article in results:
+            article_data = {
+                'API':'BBC',
+                'published_at': article['publishedAt'],
+                'title': article['title'],
+                # 'body': article['fields'].get('body', 'No body content available'),
+                'url': article['url']
+            }
+            #for testing to see what article data was fetched
+            #print(article_data)
+            articles.append(article_data)
+        return articles
+    else:
+        print(f"Failed to retrieve news. Status code: {response.status_code}")
+
 if __name__ == '__main__':
     fetch_knife_bbc()
     fetch_theft_bbc()
+    fetch_drug_bbc()
