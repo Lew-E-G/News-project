cf12f30e213e411abc3bfa8141e4f165637b9f7c
Lewis <luigizzle@gmail.com>
2024-11-08 13:41:40 +0100

    Route for drug article query added

--- app.py
+++ app.py
@@ -118,27 +118,47 @@ def theft():
     return render_template('category.html', page_title=page_title, articles=formatted_articles, current_time=current_time)
 
 # additional category to add on the backend
-# @app.route('/drugs', methods=['GET', 'POST'])
-# def drugs():
+@app.route('/drugs', methods=['GET', 'POST'])
+def drugs():
 
-    #     # Check if the user is authenticated
-    # if not session.get('authenticated'):
-    #     flash('Please log in to access this page.')
-    #     return redirect(url_for('login'))
+        # Check if the user is authenticated
+    if not session.get('authenticated'):
+        flash('Please log in to access this page.')
+        return redirect(url_for('login'))
+
+    page_title = 'Drugs'
+    #refresh button
+    if request.method == 'POST':
+        try:
+            data_refresh()  # Call the refresh function
+            flash("Data refreshed successfully!", "success")  # Success message
+        except Exception as e:
+            flash(f"An error occurred during refresh: {str(e)}", "danger")  # Error message
+
+    #database connection
+    db_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'instance', 'cleaned_articles.db')
+    conn = sqlite3.connect(db_file)
+    cursor = conn.cursor()
+
+    #query to db
+    cursor.execute("SELECT api, published_at, title, url FROM drug_table")
+    articles = cursor.fetchall()
+    formatted_articles = []
     
-    # title = 'Drugs'
-    # Refresh button
-    # if request.method == 'POST':
-    #     try:
-    #         data_refresh()  # Call the refresh function
-    #         flash("Data refreshed successfully!", "success")  # Success message
-    #     except Exception as e:
-    #         flash(f"An error occurred during refresh: {str(e)}", "danger")  # Error message
-    # #database connection
-    # db_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'instance', 'cleaned_articles.db')
-    # conn = sqlite3.connect(db_file)
-    # cursor = conn.cursor()
-    # return render_template('category.html')
+    #reformats the date column removing the time
+    for article in articles:
+        api, published_at, title, url = article
+        # Parse and format the date
+        date_obj = datetime.strptime(published_at, "%Y-%m-%dT%H:%M:%SZ")
+        formatted_date = date_obj.strftime("%d/%m/%Y")  # Format as DD/MM/YYYY
+        # Append the formatted article
+        formatted_articles.append((api, formatted_date, title, url))
+
+    conn.close()
+
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    return render_template('category.html', page_title=page_title, articles=formatted_articles, current_time=current_time)
 
 if __name__ == '__main__':
     app.run(debug=True)
